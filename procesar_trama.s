6a4ea449c9cacd790b155c333016e84b80e15fd1
geraldvindasramirez <geraldvindasramirez4@gmail.com>
2026-09-15 13:22:34 -0600

    Add files via upload

--- procesar_trama.s
+++ procesar_trama.s
@@ -198,3 +198,167 @@ mov x2, #len_grados
 
 svc #0
 
+alertas:
+
+mov x8, #64
+
+mov x0, x20
+
+ldr x1, =txt_sep_alert
+
+mov x2, #len_sep_alert
+
+svc #0
+
+ldrb w22, [x21, #4]
+
+ands w2, w22,0x0F  // bits del 0 a 3
+
+bne evaluar_bits
+
+// no alerta
+
+mov x8, #64
+
+mov x0, x20
+
+ldr x1, =txt_sin_alertas
+
+mov x2, #len_sin_alertas
+
+svc #0
+
+b fin_alertas
+
+evaluar_bits:
+
+tst w22, #0x01
+
+beq check_bit1
+
+mov x8, #64
+
+mov x0, x20
+
+ldr x1, =txt_bat_baja
+
+mov x2, #len_bat_baja
+
+svc #0
+
+check_bit1:
+
+tst w22, #0x02
+
+beq check_bit2
+
+mov x8, #64
+
+mov x0, x20
+
+ldr x1, =txt_fallo_sens
+
+mov x2, #len_fallo_sens
+
+svc #0
+
+check_bit2:
+
+tst w22, #0x04
+
+beq check_bit3
+
+mov x8, #64
+
+mov x0, x20
+
+ldr x1, =txt_fuera_rango
+
+mov x2, #len_fuera_rango
+
+svc #0
+
+check_bit3:
+
+tst w22, #0x08
+
+beq_fin_alertas
+
+mov x8, #64
+
+mov x0, x20
+
+ldr x1, =txt_calibracion
+
+mov x2, #len_calibracion
+
+svc #0
+
+fin_alertas:
+
+mov x8, #64
+
+mov x0, x20
+
+ldr x1, =txt_salto
+
+mov x2, #1
+
+svc #0
+
+mov x0, #1
+
+b salir_procesar
+
+trama_invalida:
+
+mov x0, #0
+
+salir_procesar:
+
+ldp x21, x22, [sp, #16]
+
+ldp x23, x30, [sp, #32]
+
+ldp x20, x20, [sp], #48
+
+ret
+
+escribir_num_ascii:
+stp x29, 30, [sp, #-32]!
+
+mov x29, sp
+
+add x1, sp, #31
+
+mov x2, #0
+
+mov x3, #10
+
+bucle_conv:
+
+udiv x4, x0, x3
+
+msub x5, x4, x3, x0
+
+add w5, w5, #0x30
+
+sub x1, x1, #1
+
+strb  w5, [x1]
+
+add x2, x2, #1
+
+mov x0, x4
+
+cbnz x0, bucle_conv
+
+mov x8, #64
+
+mov x0, x20
+
+svc #0
+
+ldp x29, x30, [sp], #32
+
+ret
